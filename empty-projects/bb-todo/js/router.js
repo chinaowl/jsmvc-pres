acf7f6b6691d93c62e89d17e0d5fe6ea79a23289
China Wang <chwang@thoughtworks.com>
2016-06-06 10:25:06 -0700

    started backbone todo

---
+++ empty-projects/bb-todo/js/router.js
@@ -0,0 +1,26 @@
+window.TodoApp = new (Backbone.Router.extend({
+  routes: {'': 'index'},
+
+  initialize: function () {
+    this.todoItems = new TodoItems();
+    this.todosView = new TodosView({collection: this.todoItems});
+    this.todosView.render();
+  },
+
+  index: function () {
+    var fixtures = [
+      {val: 'Shake it off', completed: true},
+      {val: 'Get out of the woods', completed: false},
+      {val: 'Welcome to New York', completed: true},
+      {val: 'Clean', completed: false},
+      {val: 'Dream wild', completed: false}
+    ];
+
+    $('#app').html(this.todosView.el);
+    this.todoItems.reset(fixtures);
+  },
+
+  start: function () {
+    Backbone.history.start();
+  }
+}))();
