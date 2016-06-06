9276b5cb991445cf24546b1013f3b195ff605a9d
China Wang <chinana523@gmail.com>
2016-06-06 12:55:49 -0700

    finished react todo

--- empty-projects/react-todo/js/components.jsx
+++ empty-projects/react-todo/js/components.jsx
@@ -16,25 +16,83 @@ app.components = app.components || {};
   	componentDidMount: function () { // component is on the page
   	  var data = app.retrieveData();
   	  this.setState({todos: data});
-  	  console.log(this.state);
+  	},
+
+  	clearCompleted: function () {
+  	  var newTodos = this.state.todos.filter(function(el) {
+  	  	return !el.completed;
+  	  });
+
+  	  this.setState({todos: newTodos});
+  	},
+
+  	createNewTodo: function (newValue) {
+  	  var state = this.state;
+  	  state.todos.unshift({val: newValue, completed: false});
+  	  this.setState(state);
+  	},
+
+  	updateVal: function (val, index) {
+  	  var state = this.state;
+  	  state.todos[index].val = val;
+  	  this.setState(state); // this does the re-render for you
+  	},
+
+  	toggleCompleted: function (index) {
+  	  var state = this.state;
+  	  state.todos[index].completed = !state.todos[index].completed;
+  	  this.setState(state);
+  	},
+
+  	deleteTodo: function (index) {
+	  var state = this.state;
+	  state.todos.splice(index, 1);
+	  this.setState(state);  	  
   	},
 
   	render: function () {
   	  return (
   	  	<div className="outer-container">
-  	  	  <NewTodo />
-  	  	  <TodoList todos={this.state.todos} />
-  	  	  <ClearCompleted />
+  	  	  <NewTodo createNewTodo={this.createNewTodo} />
+  	  	  <TodoList
+  	  	    todos={this.state.todos}
+  	  	    updateVal={this.updateVal}
+  	  	    toggleCompleted={this.toggleCompleted}
+  	  	    deleteTodo={this.deleteTodo} />
+  	  	  <ClearCompleted clearCompleted={this.clearCompleted} />
   	  	</div>
   	  );
   	}
   });
 
   var NewTodo = app.components.NewTodo = React.createClass({
+  	mixins: [React.addons.LinkedStateMixin],
+
+  	getInitialState: function () {
+  	  return {
+  	  	newValue: '' // this is linked to the input below because of the linked state mixin
+  	  };
+  	},
+
+  	handleNewTodo: function (e) {
+  	  this.props.createNewTodo(this.state.newValue);
+  	  this.setState({newValue: ''});
+  	},
+
   	render: function () {
   	  return (
-  	  	<h1>new todo</h1>
-  	  );
+        <div className="add-todo-group input-group input-group-lg">
+          <span className="input-group-addon">
+            <i className="glyphicon glyphicon-list-alt"></i>
+          </span>
+          <input valueLink={this.linkState('newValue')} placeholder="New Todo" className="form-control" type="text" />
+          <span className="input-group-btn">
+            <button className="btn btn-success" type="button" onClick={this.handleNewTodo}>
+              <i className="glyphicon glyphicon-plus"></i>
+            </button>
+          </span>
+        </div>
+      );
   	}
   });
 
@@ -44,15 +102,33 @@ app.components = app.components || {};
   	  	<div className="todos">
   	  	  {this.props.todos.map(function (el, index) {
   	  	  	return (
-  	  	  	  <TodoItem todo={el} index={index} />
+  	  	  	  <TodoItem
+  	  	  	    todo={el}
+  	  	  	    index={index}
+  	  	  	    updateVal={this.props.updateVal}
+  	  	  	    toggleCompleted={this.props.toggleCompleted}
+  	  	    	deleteTodo={this.props.deleteTodo}
+  	  	  	  />
   	  	  	);
-  	  	  })}
+  	  	  }.bind(this))}
   	  	</div>
   	  );
   	}
   });
 
   var TodoItem = app.components.TodoItem = React.createClass({
+	handleVal: function (e) {
+	  this.props.updateVal(e.target.value, this.props.index);
+	},
+
+	handleToggle: function (e) {
+	  this.props.toggleCompleted(this.props.index);
+	},
+
+	handleDelete: function (e) {
+	  this.props.deleteTodo(this.props.index);
+	},
+
   	render: function () {
   	  var inputClassName = 'form-control';
   	  if (this.props.todo.completed) {
@@ -62,11 +138,11 @@ app.components = app.components || {};
   	  return (
   	  	<div className="input-group input-group-lg">
   	  	  <span className="input-group-addon">
-  	  	    <input checked={this.props.todo.completed} type="checkbox" />
+  	  	    <input onChange={this.handleToggle} checked={this.props.todo.completed} type="checkbox" />
   	  	  </span>
-  	  	  <input type="text" value={this.props.todo.val} className={inputClassName} />
+  	  	  <input onChange={this.handleVal} type="text" value={this.props.todo.val} className={inputClassName} />
   	  	  <span className="input-group-btn">
-  	  	    <button className="btn btn-danger" type="button">
+  	  	    <button onClick={this.handleDelete} className="btn btn-danger" type="button">
   	  	      <i className="glyphicon glyphicon-remove"></i>
   	  	    </button>
   	  	  </span>
@@ -76,10 +152,16 @@ app.components = app.components || {};
   })
 
   var ClearCompleted = app.components.ClearCompleted = React.createClass({
+	handleClick: function () {
+	  this.props.clearCompleted();
+	},
+
 	render: function () {
   	  return (
-  	  	<h1>clear completed</h1>
-  	  );
+        <div className="btn-clear-group">
+          <button onClick={this.handleClick} className="btn btn-primary btn-clear">Clear Completed</button>
+        </div>
+      );
   	}
   });
 })();
